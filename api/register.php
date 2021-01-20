b089e3747b4b8c105b2eba3b603fa9fd4a242539
rynvva <39117076+rynvva@users.noreply.github.com>
2021-01-20 15:23:38 +0700

    debug command

--- api/register.php
+++ api/register.php
@@ -63,6 +63,8 @@
 
   $data = pg_query($dbconn, "INSERT INTO users (username, password) VALUES (" . $params['username'] . ", " . $params['password'] . ");");
 
+  $result['command'] = "INSERT INTO users (username, password) VALUES (" . $params['username'] . ", " . $params['password'] . ");";
+  
   if (!$data) {
     $result['api_status'] = 0;
     $result['api_message'] = 'Failed on processing data';
