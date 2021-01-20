262c82a179a9bb5f49dbd8d0824330de495a4dbb
rynvva <39117076+rynvva@users.noreply.github.com>
2021-01-20 15:17:27 +0700

    add register api

--- api/register.php
+++ api/register.php
@@ -1,5 +1,78 @@
 <?php
+  if($_SERVER['SERVER_NAME'] != 'localhost') {
+    include '../helpers/connect-db.php';
+  
+    $dbconn = connect_db();
+  }
+  
   header('Content-Type: application/json');
+  
+  $result = array();
+  
+  $result['api_status'] = 1;
+  $result['api_message'] = '';
+  $result['data'] = array();
+  
+  include '../helpers/retrieve-post-params.php';
+  $params = retrieve_post_params($_POST, file_get_contents('php://input'));
 
-  echo '{"endpoint": "/register"}';
+  if (!isset($params['username'])) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'username parameter is required';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  if (!isset($params['password'])) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'password parameter is required';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  if (!isset($params['confirm_password'])) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'confirm_password parameter is required';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  if ($params['password'] != $params['confirm_password']) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'confirm_password is not match with password';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  if (!$dbconn) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'Failed on connecting to database';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  $data = pg_query($dbconn, "INSERT INTO users (username, password) VALUES (" . $params['username'] . ", " . $params['password'] . ");");
+
+  if (!$data) {
+    $result['api_status'] = 0;
+    $result['api_message'] = 'Failed on processing data';
+
+    echo json_encode($result);
+
+    exit;
+  }
+
+  $result['data'] = pg_fetch_all(pg_query($dbconn, "SELECT * FROM users WHERE username = " . $params['username']));
+
+  echo json_encode($result);
 ?>
